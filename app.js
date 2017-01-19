fe4bb8d536cce12ae4f693ee8b8b9a83709a2e80
danielgynn <danielgynn@hotmail.co.uk>
2017-01-19 21:35:29 +0000

    Add user authentication

--- app.js
+++ app.js
@@ -6,10 +6,20 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var hbs = require('express-hbs');
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+var flash = require('connect-flash');
+var session = require('express-session');
+
 // Include routes
 var routes = require('./routes/index');
-var bar = require('./routes/bar');
-var line = require('./routes/line');
+var users = require('./routes/users');
+
+// Set config for Mongodb users
+var configDB = require('./config/database.js');
+mongoose.connect(configDB.url);
+require('./config/passport')(passport);
 
 var app = express();
 
@@ -25,10 +35,16 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
+
+app.use(session({ secret: 'shhsecret' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+// Define routes
 app.use('/', routes);
-app.use('/bar', bar);
-app.use('/line', line);
+app.use('/users', users);
 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,78 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 // GET index page
 router.get('/', function(req, res, next) {
-
   res.render('index', {
     title: 'DJG44 IoT Project',
     heading: 'JavaScript frontend for IoT',
     layout: './partials/layout',
     ws: 'ws://127.0.0.1:15675/ws',
-    host: '192.168.1.212'
+    host: '192.168.1.212',
+    user: req.user
   });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', {
+    // message: req.flash('loginMessage')
+    heading: 'Login',
+    layout: './partials/layout'
+  });
+});
+
+router.get('/signup', function(req, res) {
+  res.render('signup', {
+    // message: req.flash('loginMessage')
+    heading: 'Signup',
+    layout: './partials/layout'
+  });
+});
+
+// GET visuals data
+router.get('/line', isLoggedIn, function(req, res) {
+  res.render('line', {
+    title: 'Line Chart',
+    heading: 'Line Chart Visualisations',
+    subheading: 'Moisture level readings over time.',
+    layout: './partials/layout',
+    user: req.user
+  });
+});
+
+// GET visuals data
+router.get('/bar', isLoggedIn, function(req, res, next) {
+  res.render('bar', {
+    title: 'Bar Data Visualisations',
+    heading: 'Bar Chart Visualisations',
+    layout: './partials/layout',
+    user: req.user
+  });
+});
+
+// Unauthenticate user
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+router.post('/signup', passport.authenticate('local-signup', {
+  successRedirect: '/',
+  failureRedirect: '/signup',
+  failureFlash: true,
+}));
+
+router.post('/login', passport.authenticate('local-login', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true,
+}));
+
 module.exports = router;
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated())
+      return next();
+  res.redirect('/');
+}
