557288ada8dde01874deac169f2473ed967dbc08
danielgynn <danielgynn@hotmail.co.uk>
2017-01-20 11:05:30 +0000

    rm node modules & bower c

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
-var router = express.Router();
 var passport = require('passport');
+var router = express.Router();
 
 // GET index page
 router.get('/', function(req, res, next) {
